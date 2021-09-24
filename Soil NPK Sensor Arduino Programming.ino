7cd241ce7b3f5bb3add888644eeef81123c53b2e
Jyotishko Roy <87931405+roy14k@users.noreply.github.com>
2021-09-25 04:45:51 +0530

    Create Soil NPK Sensor Arduino Programming.ino

---
+++ Soil NPK Sensor Arduino Programming.ino
@@ -0,0 +1,487 @@
+
+/*
+ * https://www.electroniclinic.com/
+ * Soil NPK Sensor with Arduino for measuring Nitrogen, Phosphorus, and Potassium
+ */
+#include <SoftwareSerial.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// For the i2c supported Oled display module which is 128x64 
+#define SCREEN_WIDTH 128    // OLED display width, in pixels
+#define SCREEN_HEIGHT 64    // OLED display height, in pixels
+#define OLED_RESET -1       // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+ 
+#define RE 8
+#define DE 7
+
+// The following are the Inquiry frames which are send to the NPK sensor
+//for reading the Nitrogen, Phosphorus, and Potassium values
+// We defined three arrays with names nitro_inquiry_frame, phos_inquiry_frame, and pota_inquiry_frame 
+// Each inquiry frame have 8 values
+const byte nitro_inquiry_frame[] = {0x01,0x03, 0x00, 0x1e, 0x00, 0x01, 0xe4, 0x0c};
+const byte phos_inquiry_frame[] = {0x01,0x03, 0x00, 0x1f, 0x00, 0x01, 0xb5, 0xcc};
+const byte pota_inquiry_frame[] = {0x01,0x03, 0x00, 0x20, 0x00, 0x01, 0x85, 0xc0};
+ 
+byte values[11];
+SoftwareSerial modbus(2,3);
+ 
+void setup() {
+  Serial.begin(9600);
+  modbus.begin(9600);
+  pinMode(RE, OUTPUT);
+  pinMode(DE, OUTPUT);
+  
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); //initialize with the I2C addr 0x3C (128x64)
+  delay(500);
+  display.clearDisplay();
+  display.setCursor(25, 15);
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.println(" NPK Sensor");
+  display.setCursor(25, 35);
+  display.setTextSize(1);
+  display.print("Initializing");
+  display.display();
+  delay(2000);
+}
+ 
+void loop() {
+  // we will need three variables of the type byte to store the values of 
+  // Nitrogen, phosphorus, and Potassium. 
+  byte nitrogen_val,phosphorus_val,potassium_val;
+
+  nitrogen_val = nitrogen();
+  delay(250);
+  phosphorus_val = phosphorous();
+  delay(250);
+  potassium_val = potassium();
+  delay(250);
+  
+  // The following code is used to send the data to the serial monitor
+  // but as we have connected the Bluetooth module, so it will send data to the 
+  // Android cell phone Application
+  
+  Serial.print("Nitrogen_Val: ");
+  Serial.print(nitrogen_val);
+  Serial.println(" mg/kg");
+  Serial.print("Phosphorous_Val: ");
+  Serial.print(phosphorus_val);
+  Serial.println(" mg/kg");
+  Serial.print("Potassium_Val: ");
+  Serial.print(potassium_val);
+  Serial.println(" mg/kg");
+  delay(2000);
+
+ // The following code is used to display the data on the Oled display
+ //If you don't want to use the Oled display then you can simply delete the following code. 
+ // Or you can also connect the Oled display module, this way you can display the data
+ // on the Oled display module, and the data can also be send to the Android App. 
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setCursor(0, 5);
+  display.print("N: ");
+  display.print(nitrogen_val);
+  display.setTextSize(1);
+  display.print(" mg/kg");
+ 
+  display.setTextSize(2);
+  display.setCursor(0, 25);
+  display.print("P: ");
+  display.print(phosphorus_val);
+  display.setTextSize(1);
+  display.print(" mg/kg");
+ 
+  display.setTextSize(2);
+  display.setCursor(0, 45);
+  display.print("K: ");
+  display.print(potassium_val);
+  display.setTextSize(1);
+  display.print(" mg/kg");
+ 
+  display.display();
+}
+
+/*
+ * Now, we will need to create three user-defined functions to read the Nitrogen, Phosphorus, and Potassium values
+ * These user-defined functions are going to be of the type byte
+ */
+ 
+byte nitrogen(){
+  digitalWrite(DE,HIGH);
+  digitalWrite(RE,HIGH);
+  delay(10);
+  if(modbus.write(nitro_inquiry_frame,sizeof(nitro_inquiry_frame))==8){
+    digitalWrite(DE,LOW);
+    digitalWrite(RE,LOW);
+    // When we send the inquiry frame to the NPK sensor, then it replies with the response frame
+    // now we will read the response frame, and store the values in the values[] arrary, we will be using a for loop.
+    for(byte i=0;i<7;i++){
+    //Serial.print(modbus.read(),HEX);
+    values[i] = modbus.read();
+   // Serial.print(values[i],HEX);
+    }
+    Serial.println();
+  }
+  return values[4]; // returns the Nigtrogen value only, which is stored at location 4 in the array
+}
+ 
+byte phosphorous(){
+  digitalWrite(DE,HIGH);
+  digitalWrite(RE,HIGH);
+  delay(10);
+  if(modbus.write(phos_inquiry_frame,sizeof(phos_inquiry_frame))==8){
+    digitalWrite(DE,LOW);
+    digitalWrite(RE,LOW);
+    for(byte i=0;i<7;i++){
+    //Serial.print(modbus.read(),HEX);
+    values[i] = modbus.read();
+   // Serial.print(values[i],HEX);
+    }
+    Serial.println();
+  }
+  return values[4];
+}
+ 
+byte potassium(){
+  digitalWrite(DE,HIGH);
+  digitalWrite(RE,HIGH);
+  delay(10);
+  if(modbus.write(pota_inquiry_frame,sizeof(pota_inquiry_frame))==8){
+    digitalWrite(DE,LOW);
+    digitalWrite(RE,LOW);
+    for(byte i=0;i<7;i++){
+    //Serial.print(modbus.read(),HEX);
+    values[i] = modbus.read();
+    //Serial.print(values[i],HEX);
+    }
+    Serial.println();
+  }
+  return values[4];
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+83
+84
+85
+86
+87
+88
+89
+90
+91
+92
+93
+94
+95
+96
+97
+98
+99
+100
+101
+102
+103
+104
+105
+106
+107
+108
+109
+110
+111
+112
+113
+114
+115
+116
+117
+118
+119
+120
+121
+122
+123
+124
+125
+126
+127
+128
+129
+130
+131
+132
+133
+134
+135
+136
+137
+138
+139
+140
+141
+142
+143
+144
+145
+146
+147
+148
+149
+150
+151
+152
+153
+154
+155
+156
+157
+158
+159
+160
+161
+162
+/*
+ * https://www.electroniclinic.com/
+ * Soil NPK Sensor with Arduino for measuring Nitrogen, Phosphorus, and Potassium
+ */
+#include <SoftwareSerial.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+ 
+// For the i2c supported Oled display module which is 128x64 
+#define SCREEN_WIDTH 128    // OLED display width, in pixels
+#define SCREEN_HEIGHT 64    // OLED display height, in pixels
+#define OLED_RESET -1       // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+ 
+#define RE 8
+#define DE 7
+ 
+// The following are the Inquiry frames which are send to the NPK sensor
+//for reading the Nitrogen, Phosphorus, and Potassium values
+// We defined three arrays with names nitro_inquiry_frame, phos_inquiry_frame, and pota_inquiry_frame 
+// Each inquiry frame have 8 values
+const byte nitro_inquiry_frame[] = {0x01,0x03, 0x00, 0x1e, 0x00, 0x01, 0xe4, 0x0c};
+const byte phos_inquiry_frame[] = {0x01,0x03, 0x00, 0x1f, 0x00, 0x01, 0xb5, 0xcc};
+const byte pota_inquiry_frame[] = {0x01,0x03, 0x00, 0x20, 0x00, 0x01, 0x85, 0xc0};
+ 
+byte values[11];
+SoftwareSerial modbus(2,3);
+ 
+void setup() {
+  Serial.begin(9600);
+  modbus.begin(9600);
+  pinMode(RE, OUTPUT);
+  pinMode(DE, OUTPUT);
+  
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); //initialize with the I2C addr 0x3C (128x64)
+  delay(500);
+  display.clearDisplay();
+  display.setCursor(25, 15);
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.println(" NPK Sensor");
+  display.setCursor(25, 35);
+  display.setTextSize(1);
+  display.print("Initializing");
+  display.display();
+  delay(2000);
+}
+ 
+void loop() {
+  // we will need three variables of the type byte to store the values of 
+  // Nitrogen, phosphorus, and Potassium. 
+  byte nitrogen_val,phosphorus_val,potassium_val;
+ 
+  nitrogen_val = nitrogen();
+  delay(250);
+  phosphorus_val = phosphorous();
+  delay(250);
+  potassium_val = potassium();
+  delay(250);
+  
+  // The following code is used to send the data to the serial monitor
+  // but as we have connected the Bluetooth module, so it will send data to the 
+  // Android cell phone Application
+  
+  Serial.print("Nitrogen_Val: ");
+  Serial.print(nitrogen_val);
+  Serial.println(" mg/kg");
+  Serial.print("Phosphorous_Val: ");
+  Serial.print(phosphorus_val);
+  Serial.println(" mg/kg");
+  Serial.print("Potassium_Val: ");
+  Serial.print(potassium_val);
+  Serial.println(" mg/kg");
+  delay(2000);
+ 
+ // The following code is used to display the data on the Oled display
+ //If you don't want to use the Oled display then you can simply delete the following code. 
+ // Or you can also connect the Oled display module, this way you can display the data
+ // on the Oled display module, and the data can also be send to the Android App. 
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setCursor(0, 5);
+  display.print("N: ");
+  display.print(nitrogen_val);
+  display.setTextSize(1);
+  display.print(" mg/kg");
+ 
+  display.setTextSize(2);
+  display.setCursor(0, 25);
+  display.print("P: ");
+  display.print(phosphorus_val);
+  display.setTextSize(1);
+  display.print(" mg/kg");
+ 
+  display.setTextSize(2);
+  display.setCursor(0, 45);
+  display.print("K: ");
+  display.print(potassium_val);
+  display.setTextSize(1);
+  display.print(" mg/kg");
+ 
+  display.display();
+}
+ 
+/*
+ * Now, we will need to create three user-defined functions to read the Nitrogen, Phosphorus, and Potassium values
+ * These user-defined functions are going to be of the type byte
+ */
+ 
+byte nitrogen(){
+  digitalWrite(DE,HIGH);
+  digitalWrite(RE,HIGH);
+  delay(10);
+  if(modbus.write(nitro_inquiry_frame,sizeof(nitro_inquiry_frame))==8){
+    digitalWrite(DE,LOW);
+    digitalWrite(RE,LOW);
+    // When we send the inquiry frame to the NPK sensor, then it replies with the response frame
+    // now we will read the response frame, and store the values in the values[] arrary, we will be using a for loop.
+    for(byte i=0;i<7;i++){
+    //Serial.print(modbus.read(),HEX);
+    values[i] = modbus.read();
+   // Serial.print(values[i],HEX);
+    }
+    Serial.println();
+  }
+  return values[4]; // returns the Nigtrogen value only, which is stored at location 4 in the array
+}
+ 
+byte phosphorous(){
+  digitalWrite(DE,HIGH);
+  digitalWrite(RE,HIGH);
+  delay(10);
+  if(modbus.write(phos_inquiry_frame,sizeof(phos_inquiry_frame))==8){
+    digitalWrite(DE,LOW);
+    digitalWrite(RE,LOW);
+    for(byte i=0;i<7;i++){
+    //Serial.print(modbus.read(),HEX);
+    values[i] = modbus.read();
+   // Serial.print(values[i],HEX);
+    }
+    Serial.println();
+  }
+  return values[4];
+}
+ 
+byte potassium(){
+  digitalWrite(DE,HIGH);
+  digitalWrite(RE,HIGH);
+  delay(10);
+  if(modbus.write(pota_inquiry_frame,sizeof(pota_inquiry_frame))==8){
+    digitalWrite(DE,LOW);
+    digitalWrite(RE,LOW);
+    for(byte i=0;i<7;i++){
+    //Serial.print(modbus.read(),HEX);
+    values[i] = modbus.read();
+    //Serial.print(values[i],HEX);
+    }
+    Serial.println();
+  }
+  return values[4];
+}
